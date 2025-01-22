5d6224f16740d06def435ba3bad4d551efd19558
weizeyuan <wooenrico@gmail.com>
2025-01-22 11:21:54 +0800

    @ConditionalOnMissingBean

--- spring-boot-kafka/src/main/java/io/github/wooenrico/kafka/configuration/AutoKafkaConfiguration.java
+++ spring-boot-kafka/src/main/java/io/github/wooenrico/kafka/configuration/AutoKafkaConfiguration.java
@@ -3,6 +3,7 @@ package io.github.wooenrico.kafka.configuration;
 import io.github.wooenrico.kafka.KafkaProperties;
 import io.github.wooenrico.kafka.consumer.KafkaConsumerFactory;
 import io.github.wooenrico.kafka.sender.DefaultReactorKafkaSender;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,7 @@ public class AutoKafkaConfiguration {
 
     @Bean
     @Primary
+    @ConditionalOnMissingBean(KafkaConsumerFactory.class)
     public KafkaConsumerFactory getKafkaConsumerFactory(KafkaProperties kafkaProperties) {
         return new KafkaConsumerFactory(kafkaProperties);
     }
